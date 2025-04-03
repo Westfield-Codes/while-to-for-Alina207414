8b7b7a19339805882497a73943e01a4ed7c3b418
Alina207414 <alina207414@student-schoolsofwestfield.org>
2025-04-03 14:56:16 +0000

    not done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+/* Multiplication */
 
+// Create 2 variables, a & b, and set to ne random integers between 3 and 9
+var a = Math.floor(Math.random()*10-3)+3;
+var b = Math.floor(Math.random()*10-3)+3;
+// Set a variable called product to be the product of a and b
+var product = a * b;
+// Set a variable called equation to display  a * b = ??
+var quetion = 0;
+while (question < 5) {
+equation = alert(a + " * " + b + "= ??")
+// Ask the equation of the user and store the user input in a variable called answer
+answer = prompt(equation);
+equation++;
+}
+// Say "Correct!" if the answer and product match, "Incorrect!" if not.
+if (answer == product) alert("Correct!"); 
+else alert("Incorrect!");
+/* Multiplication whjile Loop */
+// Embed all this code in a While loop and run it 5 times.
+
+/* Multiplication For Loop */
+// Convert the while loop to a for loop. 
