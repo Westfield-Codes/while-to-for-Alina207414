6adc91dbd752ac5369d950d5a082c61ecb7c776f
Alina207414 <alina207414@student-schoolsofwestfield.org>
2025-04-04 14:28:21 +0000

    done

--- done.js
+++ done.js
@@ -29,3 +29,25 @@ for (let guessCount = 0;guessCount < 4;guessCount++) {
 }
 
 
+/* Multiplication */
+
+// Create 2 variables, a & b, and set to ne random integers between 3 and 9
+var question = 0;
+for (let question =0; question < 5;question++) {
+var a = Math.floor(Math.random()*(10-3))+3;
+var b = Math.floor(Math.random()*(10-3))+3;
+// Set a variable called product to be the product of a and b
+var product = a * b;
+equation = a + " * " + b + " = ??";
+// Ask the equation of the user and store the user input in a variable called answer
+answer = prompt(equation);
+if (answer == product) alert("Correct!"); 
+else alert("Incorrect!");
+}
+// Say "Correct!" if the answer and product match, "Incorrect!" if not.
+
+/* Multiplication whjile Loop */
+// Embed all this code in a While loop and run it 5 times.
+
+/* Multiplication For Loop */
+// Convert the while loop to a for loop. 
